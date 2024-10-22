cf43d93cd3ac89d93dc1389ce8030877f04aaaee
VirtualMachine01 <tempvm12@gmail.com>
2024-10-22 12:09:38 +0530

    final touch

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask
 from modules.routes import init_routes
 from flask_cors import CORS
+from modules.config_loaders import load_config
 
 # Create Flask app
 app = Flask(__name__)
 CORS(app)
-
+config = load_config()
 # Initialize routes
 init_routes(app)
 
 if __name__ == "__main__":
-    app.run(debug=True,port=5000)
+    app.run(debug=True,port=config["port_number"])
     # app.run(host='0.0.0.0', port=5000, debug=True)
     # debug=False - for production environment
 
--- modules/routes.py
+++ modules/routes.py
@@ -5,6 +5,7 @@ from modules.model_initializer import initialize_models
 from modules.image_processing import extract_text, generate_caption
 from modules.text_processing import translate_text, generate_tokens_from_caption, contains_chinese
 from paddleocr import PaddleOCR
+import magic
 import logging
 ppocr_logger = logging.getLogger('ppocr')
 ppocr_logger.setLevel(logging.WARNING) 
@@ -19,6 +20,22 @@ result_json = {
     config["result_json_keys"]["image_ocr_text_key"]: ""
 }
 
+def is_image(file):
+    # Create a Magic object to check the MIME type
+    mime = magic.Magic(mime=True)
+    # Get the MIME type from the file's bytes
+    file_type = mime.from_buffer(file.read(1024))
+    # Reset file pointer to the beginning after reading
+    file.seek(0)
+    # Check if the MIME type starts with 'image/'
+    return file_type.startswith('image/')
+
+def contains_novideo(lst):
+    # Normalize the variations of "novideo" by removing spaces and converting to lowercase
+    normalized_list = [''.join(item.lower().split()) for item in lst]
+    # Check if any normalized item matches "novideo"
+    return 'novideo' not in normalized_list
+
 def init_routes(app):
     @app.route('/upload', methods=['POST'])
     def upload():
@@ -29,17 +46,23 @@ def init_routes(app):
         if file.filename == '':
             return jsonify({"error": "No selected file"}), 400
 
-        if file and file.filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+        if file and is_image(file):
+        # if file and file.filename.lower().endswith(('.png', '.jpg', '.jpeg')):
             image = Image.open(file).convert('RGB')
-            
-            # Image captioning
-            generated_text = generate_caption(processor, model, image, device)
-            tokens_from_caption = generate_tokens_from_caption(nlp, generated_text)
             
             # OCR and Translation
             ocr = PaddleOCR(use_angle_cls=True, lang=config["ocr_language"])
             ocr_text = extract_text(ocr, image)
 
+            if contains_novideo(ocr_text):
+                # Image captioning
+                print("Video is There")
+                generated_text = generate_caption(processor, model, image, device)
+                tokens_from_caption = generate_tokens_from_caption(nlp, generated_text)
+            else:
+                generated_text = "Video is not available"
+                tokens_from_caption = ["No Video", "Null"]
+            
             print("OCR Text:", ocr_text)
 
             # Translate only the elements that contain Chinese characters
@@ -60,4 +83,4 @@ def init_routes(app):
 
             return result_json
         else:
-            return jsonify({"error": "Invalid file type"}), 400
+            return jsonify({f"{config["result_json_keys"]["image_id_key"]}":file.filename,"error": "Invalid file type"}), 400
